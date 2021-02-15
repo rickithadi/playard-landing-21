91583ef70d5547685675873bb2f4e347f98314b1
hadi <rickithadi@gmail.com>
2021-02-16 00:20:51 +0800

    fuck

---
+++ app/pages/Kick.vue
@@ -0,0 +1,31 @@
+template>
+  <div class="page-transition--biker">
+    <div
+      v-for="index in 5"
+      :key="index"
+      :class="`biker-${index}`"
+      class="biker">
+      <Lottie
+        :options="bikerAnimation"
+        class="_animation" />
+    </div>
+  </div>
+</template>
+
+<script>
+import animationBiker from '@/assets/soccer.json';
+import Lottie from './Lottie.vue';
+
+export default {
+  components: {
+    Lottie,
+  },
+  data() {
+    return {
+      bikerAnimation: {
+        animationData: animationBiker,
+      },
+    };
+  },
+};
+</script>
