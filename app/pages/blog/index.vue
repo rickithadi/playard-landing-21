7b7c5e46a6290243ded854b70a73115849f9ab7b
hadi <rickithadi@gmail.com>
2021-02-16 11:57:36 +0800

    updated landing padding

--- app/pages/blog/index.vue
+++ app/pages/blog/index.vue
@@ -3,7 +3,7 @@
     <div class="py-8 md:py-16 text-center">
       <h1 class="text-lg md:text-xl lg:text-4xl xl:text-6xl">Blog</h1>
       <h2 class="text-base md:text-lg lg:text-xl xl:text-2xl">
-        Slow-carb messenger bag mlkshk fingerstache four dollar toast.
+        Get up to date information on the local sports scene.
       </h2>
     </div>
 
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
     <section class="home">
-      <div class="py-24 md:py-36 mx-auto flex flex-wrap flex-col md:flex-row items-center">
+      <div
+        class="py-24 md:py-36 mx-auto flex flex-wrap flex-col md:flex-row items-center"
+        style="padding-top: 0px;"
+      >
         <div class="flex flex-col w-full xl:w-3/5 justify-center lg:items-start overflow-y-hidden">
           <div v-html="$md.render(welcomeText)" class="home__welcome markdown" />
 
