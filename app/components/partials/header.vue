8c67a0fac48fbac63c37a59afd774c2afd0eb16d
hadi <rickithadi@gmail.com>
2021-03-22 10:19:15 +0800

    disabled login

--- app/components/partials/header.vue
+++ app/components/partials/header.vue
@@ -23,12 +23,8 @@
 
         <li>
           <button
-            class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
-          >
-            <a href="https://app.playard.sg" target="_blank">
-              Log In
-            </a>
-          </button>
+            class="bg-blue-500 text-white font-bold py-2 px-4 rounded opacity-50 cursor-not-allowed"
+          ></button>
         </li>
       </ul>
     </nav>
