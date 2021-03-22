30ac4fefacb7d7751f87b582cef9211aaa61f9b7
hadi <rickithadi@gmail.com>
2021-03-22 18:44:52 +0800

    y it no go

--- app/pages/contact.vue
+++ app/pages/contact.vue
@@ -1,81 +1,82 @@
 <template>
   <section class="page">
     <h1 class="page__title text-lg md:text-xl lg:text-4xl xl:text-6xl text-center py-8 md:py-16">
-      Get in Touch
+      Contact us
     </h1>
 
-    <div class="mb-12 xl:mb-0">
-      <h4 v-if="isSignedUp">Thank you - we'll be in touch shortly.</h4>
+    <h4 v-if="isSignedUp">Thank you - we'll be in touch shortly.</h4>
 
-      <form
-        v-else
-        @submit.prevent="handleSubmit"
-        name="contact"
-        netlify
-        netlify-honeypot="bot-field"
-        class="w-full max-w-lg"
-        lass="form bg-white p-6 my-10 relative"
-      >
-        <div
-          class="icon bg-blue-600 text-white w-6 h-6 absolute flex items-center justify-center p-5"
-          style="left: -40px;"
-        >
-          <i class="fal fa-phone-volume fa-fw text-2xl transform -rotate-45"></i>
-        </div>
-        <h3 class="text-2xl text-gray-900 font-semibold">Get in touch!</h3>
-        <p class="text-gray-600">To help you choose your property</p>
-        <div class="flex space-x-5 mt-3">
-          <input
-            type="text"
-            name=""
-            id=""
-            aria-label="playard"
-            placeholder="Your Name"
-            class="border p-2 w-1/2"
-          />
-          <input
-            aria-label="playard"
-            type="tel"
-            name=""
-            id=""
-            placeholder="Subject"
-            class="border p-2 w-1/2"
-          />
-        </div>
+    <form v-else @submit.prevent="handleSubmit" name="contact" netlify netlify-honeypot="bot-field">
+      <p class="hidden">
+        <input name="bot-field" />
+      </p>
+
+      <div
+        class="icon bg-blue-600 text-white w-6 h-6 absolute flex items-center justify-center p-5"
+        style="left: -40px;"
+      ></div>
+      <h3 class="text-2xl text-gray-900 font-semibold">Get in touch!</h3>
+      <p class="text-gray-600">Happy to assist with enquiries</p>
+      <div class="flex space-x-5 mt-3">
+        <input
+          type="text"
+          required
+          v-model="form.name"
+          name="name"
+          id=""
+          aria-label="playard"
+          placeholder="Your Name"
+          class="border p-2 w-1/2"
+        />
+        <input
+          aria-label="playard"
+          type="text"
+          name="subject"
+          required
+          v-model="form.subject"
+          id=""
+          placeholder="Subject"
+          class="border p-2 w-1/2"
+        />
         <input
           aria-label="playard"
           type="email"
-          name=""
+          ref="emailInput"
+          name="email"
+          required
+          v-model="form.email"
           id=""
           placeholder="Your Email"
-          class="border p-2 w-full mt-3"
+          class="border p-2 w-1/2"
         />
-        <textarea
-          name=""
-          id=""
-          aria-label="playard"
-          cols="10"
-          rows="3"
-          placeholder="Tell us about desired property"
-          class="border p-2 mt-3 w-full"
-        ></textarea>
-        <p class="font-bold text-sm mt-3">GDPR Agreement *</p>
-        <div class="flex items-baseline space-x-2 mt-2">
-          <input aria-label="playard" type="checkbox" name="" id="" class="inline-block" />
-          <p class="text-gray-600 text-sm">
-            I consent to having this website store my submitted information so they can respond to
-            my inquiry.
-          </p>
-        </div>
+      </div>
+      <textarea
+        name="query"
+        id=""
+        aria-label="playard"
+        cols="10"
+        rows="3"
+        placeholder="How can we help?"
+        required
+        v-model="form.query"
+        class="border p-2 mt-3 w-full"
+      ></textarea>
+      <p class="font-bold text-sm mt-3">GDPR Agreement *</p>
+      <div class="flex items-baseline space-x-2 mt-2">
+        <input aria-label="playard" type="checkbox" id="" class="inline-block" required />
+        <p class="text-gray-600 text-sm">
+          I consent to having this website store my submitted information so they can respond to my
+          inquiry.
+        </p>
+      </div>
 
-        <button
-          class="flex-shrink-0 bg-blue-500 hover:bg-blue-700 border-blue-500 hover:border-blue-700 text-sm border-4 text-white py-1 px-2 rounded"
-          type="submit"
-        >
-          Submit
-        </button>
-      </form>
-    </div>
+      <button
+        class="flex-shrink-0 bg-blue-500 hover:bg-blue-700 border-blue-500 hover:border-blue-700 text-sm border-4 text-white py-1 px-2 rounded p-2"
+        type="submit"
+      >
+        Submit
+      </button>
+    </form>
   </section>
 </template>
 
@@ -85,6 +86,7 @@ import { Vue } from 'nuxt-property-decorator';
 export default class Contact extends Vue {
   form = {
     email: '',
+    name: '',
     subject: '',
     query: '',
   };
@@ -104,6 +106,7 @@ export default class Contact extends Vue {
   }
 
   async handleSubmit(): Promise<void> {
+    console.log('fuc', this.form);
     if (!this.validEmail(this.form.email)) {
       this.$refs.emailInput.focus();
       return;
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -34,7 +34,7 @@
               />
 
               <button
-                class="flex-shrink-0 bg-blue-500 hover:bg-blue-700 border-blue-500 hover:border-blue-700 text-sm border-4 text-white py-1 px-2 rounded"
+                class="flex-shrink-0 bg-blue-500 hover:bg-blue-700 border-blue-500 hover:border-blue-700 text-sm border-4 text-white py-1 px-2 pb-5 rounded"
                 type="submit"
               >
                 I'm keen
