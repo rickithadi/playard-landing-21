b6fa4b52a0710fe5af72058f52729827a43a102e
hadi <rickithadi@gmail.com>
2021-03-22 13:42:24 +0800

    whitespace issue with eslint

--- app/components/partials/header.vue
+++ app/components/partials/header.vue
@@ -24,7 +24,9 @@
         <li>
           <button
             class="bg-blue-500 text-white font-bold py-2 px-4 rounded opacity-50 cursor-not-allowed"
-          > Log in</button>
+          >
+            Log in
+          </button>
         </li>
       </ul>
     </nav>
