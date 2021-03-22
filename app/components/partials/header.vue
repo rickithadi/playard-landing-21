42ce58b2aaf960ec7e1544b5976fe1de41987769
hadi <rickithadi@gmail.com>
2021-03-22 10:54:34 +0800

    disabled login

--- app/components/partials/header.vue
+++ app/components/partials/header.vue
@@ -24,7 +24,7 @@
         <li>
           <button
             class="bg-blue-500 text-white font-bold py-2 px-4 rounded opacity-50 cursor-not-allowed"
-          ></button>
+          > Log in</button>
         </li>
       </ul>
     </nav>
