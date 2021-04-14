5ecf92c990b53c32c9f44a6313b2ea9f4abdbb60
hadi <rickithadi@gmail.com>
2021-04-14 19:01:50 +0800

    updated social links

--- app/components/partials/footer.vue
+++ app/components/partials/footer.vue
@@ -1,13 +1,16 @@
 <template>
   <footer class="footer -mx-4 md:mx-0" style="width: 100vw;">
     <div class="container mx-auto pb-6 flex justify-center">
-      <a href="https://linkedin.com/company/playard" class="social">
+      <a href="https://twitter.com/playard_sg" class="social" target="_blank">
         <img style="height: 50px; weight: 50px;" alt="linkedin" src="@/assets/twitter.svg" />
       </a>
-      <a href="https://linkedin.com/company/playard" class="social">
+      <a href="https://linkedin.com/company/playard" class="social" target="_blank">
         <img style="height: 50px; weight: 50px;" alt="linkedin" src="@/assets/linkedin.svg" />
       </a>
-      <a href="https://linkedin.com/company/playard" class="social">
+      <a href="https://instagram.com/playard_sg" class="social" target="_blank">
+        <img style="height: 50px; weight: 50px;" alt="linkedin" src="@/assets/insta.svg" />
+      </a>
+      <a href="https://t.me/playardsg" class="social" target="_blank">
         <img style="height: 50px; weight: 50px;" alt="linkedin" src="@/assets/tele.svg" />
       </a>
     </div>
